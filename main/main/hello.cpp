979c391b6d25460af468996ad771e9017881204e
Athmica <athmicakishorek@gmail.com>
2025-03-12 00:10:24 +0530

    Fixed Dockerfile issue and added new changes

---
+++ main/main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "This is a new change to trigger Jenkins build\n";  // New line added
+  return 0;
+}
